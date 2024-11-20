c75bb11e7443835c0b91cf2f0ad5d64927877534
artxnv <artxnv@gmail.com>
2024-11-20 18:09:18 +0100

    refactoring

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,35 +1,41 @@
 from flask import Flask, request, render_template, redirect, url_for, flash
-from .db import (open_db_connection, close_db_connection, get_url_by_id,
-                 fetch_and_parse_url, insert_url_check, check_url_exists,
-                 insert_new_url, get_all_urls, get_url_details)
+from .db import (get_db_connection, get_url_by_id, insert_url_check,
+                 check_url_exists, insert_new_url, get_all_urls, get_url_details,
+                 configure_database_url)
 import validators
 from dotenv import load_dotenv
 import os
 from .utils import format_date, normalize_url
+from .utils import fetch_and_parse_url
 
 
+# Загрузка переменных окружения
 load_dotenv()
+
+# Настройка приложения
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback-secret-key')
 app.jinja_env.filters['date'] = format_date
 
+# Передача конфигурации базы данных
+configure_database_url(os.getenv('DATABASE_URL'))
+
 
 @app.route('/urls/<int:id>/checks', methods=['POST'])
 def create_check(id):
-    conn = open_db_connection()[0]
-    url = get_url_by_id(conn, id)
+    with get_db_connection() as conn:
+        url = get_url_by_id(conn, id)
 
-    if url:
-        result = fetch_and_parse_url(url)
-        if 'error' not in result:
-            insert_url_check(conn, id, result)
-            flash('Страница успешно проверена', 'alert-success')
+        if url:
+            result = fetch_and_parse_url(url)
+            if 'error' not in result:
+                insert_url_check(conn, id, result)
+                flash('Страница успешно проверена', 'alert-success')
+            else:
+                flash(result['error'], 'alert-danger')
         else:
-            flash(result['error'], 'alert-danger')
-    else:
-        flash('URL не найден', 'alert-danger')
+            flash('URL не найден', 'alert-danger')
 
-    close_db_connection(conn, None)
     return redirect(url_for('url_details', id=id))
 
 
@@ -46,34 +52,34 @@ def add_url():
         return render_template('index.html'), 422
 
     normalized_url = normalize_url(raw_url)
-    conn, cur = open_db_connection()
-    try:
-        existing_url = check_url_exists(cur, normalized_url)
-        if existing_url:
-            flash('Страница уже существует', 'alert-info')
-            redirect_url = redirect(url_for('url_details', id=existing_url['id']))
-        else:
-            url_id = insert_new_url(cur, normalized_url)
-            conn.commit()
-            flash('Страница успешно добавлена', 'alert-success')
-            redirect_url = redirect(url_for('url_details', id=url_id))
-    except Exception as e:
-        conn.rollback()
-        flash(f'Произошла ошибка при добавлении URL: {e}', 'alert-danger')
-        redirect_url = render_template('index.html'), 422
-    finally:
-        close_db_connection(conn, cur)
+    with get_db_connection() as conn:
+        try:
+            existing_url = check_url_exists(conn, normalized_url)
+            if existing_url:
+                flash('Страница уже существует', 'alert-info')
+                redirect_url = redirect(url_for('url_details', id=existing_url['id']))
+            else:
+                url_id = insert_new_url(conn, normalized_url)
+                conn.commit()
+                flash('Страница успешно добавлена', 'alert-success')
+                redirect_url = redirect(url_for('url_details', id=url_id))
+        except Exception as e:
+            conn.rollback()
+            flash(f'Произошла ошибка при добавлении URL: {e}', 'alert-danger')
+            redirect_url = render_template('index.html'), 422
 
     return redirect_url
 
 
 @app.route('/urls')
 def urls():
-    urls_data = get_all_urls()
+    with get_db_connection() as conn:
+        urls_data = get_all_urls(conn)
     return render_template('urls.html', urls=urls_data)
 
 
 @app.route('/urls/<int:id>')
 def url_details(id):
-    url_data, checks = get_url_details(id)
+    with get_db_connection() as conn:
+        url_data, checks = get_url_details(conn, id)
     return render_template('url.html', url=url_data, checks=checks)
--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlparse, urlunparse
+import requests
+from bs4 import BeautifulSoup
 
 
 def format_date(value, format='%Y-%m-%d'):
@@ -11,3 +13,22 @@ def normalize_url(input_url):
     url_parts = urlparse(input_url)
     normalized_url = urlunparse((url_parts.scheme, url_parts.netloc, '', '', '', ''))
     return normalized_url
+
+
+def fetch_and_parse_url(url):
+    """Загружает страницу по URL и извлекает её содержимое."""
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            return {
+                'title': soup.find('title').text if soup.find('title') else None,
+                'h1': soup.find('h1').text if soup.find('h1') else None,
+                'description': soup.find('meta', attrs={'name': 'description'})['content']
+                if soup.find('meta', attrs={'name': 'description'}) else None,
+                'status_code': response.status_code
+            }
+        else:
+            return {'error': 'Произошла ошибка при проверке'}
+    except requests.RequestException:
+        return {'error': 'Произошла ошибка при проверке'}
